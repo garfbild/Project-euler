d8237002233fd998082b8dfc3af21c2d53b46f42
garfbild <30866252+garfbild@users.noreply.github.com>
2020-06-29 23:20:50 +0100

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,16 +1,15 @@
 def Factors(n):
     factors = 0
-    for i in range(1,int(n)):
+    for i in range(1,int(n/2 + 1)):
         if n/i == int(n/i):
             factors+=1
     return factors
 
-trinumber = 1
-n = 1
+trinumber = 0
+n = 0
 numberFactors = 0
-while numberFactors < 500:
-    numberFactors = Factors(trinumber)
-    print(numberFactors)
-    trinumber = trinumber + n
+while numberFactors <500:
     n+=1
+    trinumber = trinumber + n
+    numberFactors = Factors(trinumber)
 print(trinumber)
