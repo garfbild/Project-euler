e12f299effeb1bd3d18b0d1d40321df24cd0e962
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-01 23:59:25 +0100

    Update 23.py

--- solutions/23.py
+++ solutions/23.py
@@ -1,12 +1,13 @@
 def divisors(n):
-    divisorlist = []
-    for d in range(1,int(n/2)+1):
+    divisorlist = [1]
+    for d in range(2,int(n**0.5)+1):
         if n%d == 0 and d!=n:
             divisorlist.append(d)
+        if d != n/d:
+            divisorlist.append(int(n/d))
     return divisorlist
 
 abundant = []
-
 for i in range(1,28124):
     s = 0
     for divisor in divisors(i):
@@ -14,5 +15,14 @@ for i in range(1,28124):
     if s > i:
         abundant.append(i)
 
-print(len(abundant))
-print(abundant)
+s = 0
+for i in range(1,28124):
+    if i % 10 == 0:
+        print(i,s)
+    for j in range(len(abundant)):
+        if i - abundant[j] in abundant:
+            print(i,abundant[j])
+        else:
+            s+=i
+
+print(s)
