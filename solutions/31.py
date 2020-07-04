da80423f12f1b6fddead19eda8ec25afcb0b29fc
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-04 06:41:30 +0100

    Update 31.py

--- solutions/31.py
+++ solutions/31.py
@@ -1,12 +1,11 @@
-coins = [200,100,50,20,5,2,1]
+coins = [200,100,50,20,10,5,2,1]
 target = 200
 def findSolutions(n,coins,s):
     if n == 0:
         return s + 1
-    for coin in coins:
-        if n - coin >= 0:
-            print(coin)
-            s += findSolutions(n-coin,coins,s)
+    for c in range(len(coins)):
+        if n - coins[c] >= 0:
+            s = findSolutions(n-coins[c],coins[c:],s)
     return s
 
-print(findSolutions(10,coins,0))
+print(findSolutions(200,coins,0))
