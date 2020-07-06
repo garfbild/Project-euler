7043c90b920e418c1752c29f01e8d6a9d33a9325
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-07 00:41:48 +0100

    Update 81.py

--- solutions/81.py
+++ solutions/81.py
@@ -19,22 +19,23 @@ for x in range(n):
         temp.append(int(strmatrix[x][y]))
     matrix.append(temp)
 
-n = 4
-matrix = []
-for x in range(n):
-    temp = []
-    for y in range(n):
-        temp.append(random.randint(1,10))
-    matrix.append(temp)
-[print(row) for row in matrix]
-print()
+
 for j in range(n-1,0,-1):
     matrix[n-1][j-1] += matrix[n-1][j]
     matrix[j-1][n-1] += matrix[j][n-1]
 
-layer = [[n-1,n-1]]
-for l in range(2*(n-2)):
-    print(l)
+layer = [[n-2,n-2]]
+for l in range(2*(n-2)+1):
+    nextlayer = []
+    for cell in layer:
+        if matrix[cell[0]+1][cell[1]] < matrix[cell[0]][cell[1]+1]:
+            matrix[cell[0]][cell[1]] += matrix[cell[0]+1][cell[1]]
+        else:
+            matrix[cell[0]][cell[1]] += matrix[cell[0]][cell[1]+1]
+        for adj in adjacents(cell):
+            if adj not in nextlayer:
+                nextlayer.append(adj)
+    layer = nextlayer
 
 
 [print(row) for row in matrix]
