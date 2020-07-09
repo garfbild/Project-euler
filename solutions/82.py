430f828789c031dd395676945aaf4c8883fd93ef
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-09 03:48:47 +0100

    Update 82.py

--- solutions/82.py
+++ solutions/82.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 file = open("solutions/matrix.txt","r")
 strmatrix = [row.split(",") for row in file.read().split('\n')]
@@ -10,27 +11,14 @@ for x in range(n):
         temp.append(int(strmatrix[x][y]))
     matrix.append(temp)
 
-n = 3
-matrix = []
-for x in range(n):
-    temp = []
-    for y in range(n):
-        temp.append(random.randint(1,10))
-    matrix.append(temp)
-
-[print(row) for row in matrix]
-
 for i in range(n-2,-1,-1):
-    temp = matrix
+    temp = copy.deepcopy(matrix)
     for j in range(n):
-        target = matrix[j][i]
-        print(target,":")
-        min = matrix[j][i+1]
+        min = int(matrix[j][i+1])
 
         top = 0
         row = j
         while row - 1 >= 0:
-            print(matrix[row-1][i])
             top += matrix[row-1][i]
             if top + matrix[row-1][i+1] < min:
                 min = top + matrix[row-1][i+1]
@@ -40,10 +28,16 @@ for i in range(n-2,-1,-1):
         row = j
         while row + 1 <= n - 1:
             bottom += matrix[row+1][i]
-            if top + matrix[row+1][i+1] < min:
-                min = top + matrix[row+1][i+1]
+            if bottom + matrix[row+1][i+1] < min:
+                min = bottom + matrix[row+1][i+1]
             row = row + 1
         temp[j][i] += min
 
+    matrix = temp
 
-[print(row) for row in matrix]
+
+min = matrix[i][0]
+for i in range(n):
+    if matrix[i][0] < min:
+        min = matrix[i][0]
+print(min)
