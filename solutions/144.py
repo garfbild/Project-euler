0bcb49c21be41282e5e9b11ae1b6b4cd246876a1
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-09 21:55:41 +0100

    Update 144.py

--- solutions/144.py
+++ solutions/144.py
@@ -3,13 +3,15 @@ def dotProduct(a,b):
 
 laserstart = [0.0,10.1]
 impact = [1.4,-9.6]
-laserdir = [impact[1]-laserstart[1],impact[0]-laserstart[0]]
+laserdir = [impact[0]-laserstart[0],impact[1]-laserstart[1]]
+print(laserdir)
 wallgradient = -4*impact[0]/impact[1]
+print(wallgradient)
 inverse = -1/wallgradient
-print(inverse)
-#compute normal
 
+#compute normal
 normal = [1,inverse]
+print(normal)
 reflection = [laserdir[0] - 2*dotProduct(laserdir,normal)*normal[0],laserdir[1] - 2*dotProduct(laserdir,normal)*normal[1]]
 print(reflection)
 print(reflection[1]/reflection[0])
