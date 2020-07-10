9dc0411cb0a2f027c8812543b6a4930a4f79089a
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-11 00:45:22 +0100

    Update 144.py

--- solutions/144.py
+++ solutions/144.py
@@ -17,3 +17,12 @@ normNormal = normal / LA.norm(normal)
 #compute reflection vector
 reflection = laserdir - 2*np.dot(laserdir,normNormal)*normNormal
 print(reflection)
+
+#intersection
+a = reflection[0]**2 * 4*reflection[1]**2
+b = 2 * impact[0] * reflection[0] + 4 * impact[1] * reflection[1]
+c = impact[0]**2 + 4 * impact[1] - 100
+print(a,b,c)
+Lambda = np.roots([a,b,c])
+print(reflection * Lambda[1] + impact)
+print(reflection * Lambda[0] + impact)
