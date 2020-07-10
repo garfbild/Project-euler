15efc54a64ed901ea0a7ab4d5c4ee15e5e2ec131
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-10 06:38:38 +0100

    Update 144.py

--- solutions/144.py
+++ solutions/144.py
@@ -4,12 +4,16 @@ from numpy import linalg as LA
 laserstart = np.array([0.0,10.1])
 impact = np.array([1.4,-9.6])
 laserdir = impact - laserstart
+
+#create tangent vector
 wallgradient = -4*impact[0]/impact[1]
 wallVect = np.array([1,wallgradient])
 
-#compute normal
+#compute normalised normal
 rotationMat = np.array([[0,-1],[1,0]])
 normal = np.matmul(rotationMat, wallVect)
 normNormal = normal / LA.norm(normal)
+
+#compute reflection vector
 reflection = laserdir - 2*np.dot(laserdir,normNormal)*normNormal
 print(reflection)
