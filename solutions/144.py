9db6d48c4be9cb048b31f627a3c35ccc6e412238
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-11 01:00:03 +0100

    Update 144.py

--- solutions/144.py
+++ solutions/144.py
@@ -19,10 +19,5 @@ reflection = laserdir - 2*np.dot(laserdir,normNormal)*normNormal
 print(reflection)
 
 #intersection
-a = reflection[0]**2 * 4*reflection[1]**2
-b = 2 * impact[0] * reflection[0] + 4 * impact[1] * reflection[1]
-c = impact[0]**2 + 4 * impact[1] - 100
-print(a,b,c)
-Lambda = np.roots([a,b,c])
-print(reflection * Lambda[1] + impact)
-print(reflection * Lambda[0] + impact)
+c = impact[1] - reflection[1]/reflection[0] * impact[0]
+print(c)
