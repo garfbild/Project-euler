53d30b06b494c926028fca5dfcdbc08d33634b0d
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-12 09:35:28 +0100

    Update 144.py

--- solutions/144.py
+++ solutions/144.py
@@ -1,16 +1,22 @@
 import numpy as np
 from numpy import linalg as LA
-# 0 is x 1 is y
+from matplotlib import pyplot as plt
+from decimal import *
+import time
 
-laseremit = np.array([0.0,10.1])
-laserimpact = np.array([1.4,-9.6])
+getcontext().prec = 50
 
-i = 0
+# 0 is x, 1 is y
+
+laseremit = np.array([Decimal(0.0),Decimal(10.1)])
+laserimpact = np.array([Decimal(1.4),Decimal(-9.6)])
+
+i = 1
 bol = True
 while bol:
     laserdir = laserimpact - laseremit
     #create tangent vector
-    wallgradient = -4*laserimpact[0]/laserimpact[1]
+    wallgradient = Decimal(-4*laserimpact[0]/laserimpact[1])
     wallVect = np.array([1,wallgradient])
 
     #compute normalised normal
@@ -21,7 +27,6 @@ while bol:
     #compute reflection vector
     reflection = laserdir - 2*np.dot(laserdir,normNormal)*normNormal
 
-
     #intersection
     m = reflection[1]/reflection[0]
     c = laserimpact[1] - m * laserimpact[0]
@@ -31,13 +36,12 @@ while bol:
     a3 = (c**2) - 100
     roots = np.roots([a1,a2,a3])
 
-    if abs(roots[0]-laserimpact[0]) < 0.01:
-        x = roots[1]
+    if abs(Decimal(roots[0])-laserimpact[0]) < 0.01:
+        x = Decimal(roots[1])
     else:
-        x = roots[0]
-    Lambda = (x - laserimpact[0])/reflection[0]
-    y = reflection[1]*Lambda + laserimpact[1]
-
+        x = Decimal(roots[0])
+    Lambda = Decimal((x - laserimpact[0])/reflection[0])
+    y = Decimal(reflection[1]*Lambda + laserimpact[1])
 
     laseremit = laserimpact
     laserimpact = np.array([x,y])
