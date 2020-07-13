2f96e2262855010cc0985099ea649c76c4c36925
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-13 07:25:49 +0100

    Update 182.py

--- solutions/182.py
+++ solutions/182.py
@@ -1,3 +1,11 @@
+#for d in range(1,phi - 1):
+    #if e*d % phi == 1:
+        #ds.append(d)
+#if ds != []:
+    #saved.append([e,ds])
+
+import time
+
 def GCD(a,b):
     #a > b
     if a == b:
@@ -17,16 +25,27 @@ def GCD(a,b):
         b = r
     return r
 
-p = 19
-q = 37
+p = 1009
+q = 3643
 n = p*q
+print(n)
 phi = (p-1)*(q-1)
 
-es = []
+saved = []
+min = 99999
 for e in range(1,phi-1):
     if GCD(e,phi) == 1:
-        es.append(e)
-
-m = 19
-c = m**e % n
-m = c**
+        print("e:",e)
+        s = 0
+        m = 0
+        t = time.time()
+        while s <= min and m <= n-1:
+            c = m**e % n
+            if c == m:
+                s+=1
+            m+=1
+        print("time",time.time() - t)
+        if s < min:
+            min = s
+            print("min",min)
+        saved.append([e,s])
