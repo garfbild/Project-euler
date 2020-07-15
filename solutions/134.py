108b710a0ca27ab23bcb71eca974c10a35c6e945
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-15 17:39:28 +0100

    Update 134.py

--- solutions/134.py
+++ solutions/134.py
@@ -1,3 +1,4 @@
+#not finised
 def Sieve(n):
     sieve = [0] * (n+1)
     for d in range(2,int(n**0.5)):
@@ -15,3 +16,17 @@ def Sieve(n):
 
 primes = Sieve(1000000)
 print(len(primes))
+S = 0
+for x in range(10):
+    if x%100 == 0:
+        print(x)
+    if x != 1:
+        p1 = primes[x]
+        p2 = primes[x+1]
+        prefix = 1
+        power = len(str(p1))
+        while (prefix*(10**power) + p1) % p2 != 0:
+            prefix += 1
+        print(p1,p2,prefix*(10**power) + p1)
+        S += prefix*100 + p1
+print(S)
