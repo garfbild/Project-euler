3197158f293f89a6e7af1353699fee7261a4b1d5
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-26 18:18:07 +0100

    Update 96.py

--- solutions/96.py
+++ solutions/96.py
@@ -4,7 +4,7 @@ List = file.read().split('\n')
 g = 0
 old_sudoku = List[10*g + 1:10*(g+1)]
 new_sudoku = []
-empty_calls = 0
+empty_cells = 0
 for i in range(9):
     old_row = list(old_sudoku[i])
     new_row = []
@@ -12,22 +12,32 @@ for i in range(9):
         new_row.append(int(old_row[j]))
         if int(old_row[j]) == 0:
             empty_cells += 1
-    print(new_row)
     new_sudoku.append(new_row)
-print()
 
-for y in range(9):
-    for x in range(9):
-        neighbours = []
-        for n in range(9):
-            if new_sudoku[y][n] != 0 and new_sudoku[y][n] not in neighbours:
-                neighbours.append(new_sudoku[y][n])
-            if new_sudoku[n][x] != 0 and new_sudoku[n][x] not in neighbours:
-                neighbours.append(new_sudoku[n][x])
-        if new_sudoku[y][x] == 0:
-            temp = []
-            for m in range(1,10):
-                if m not in neighbours:
-                    temp.append(m)
-            if len(temp) == 1:
-                new_sudoku[y][x] = temp[0]
+for k in range(15):
+    [print(row) for row in new_sudoku]
+    print(empty_cells)
+    print()
+    y = 0
+    while y <= 8:
+        x=0
+        while x <= 8:
+            neighbours = []
+            for n in range(9):
+                if new_sudoku[y][n] != 0 and new_sudoku[y][n] not in neighbours:
+                    neighbours.append(new_sudoku[y][n])
+                if new_sudoku[n][x] != 0 and new_sudoku[n][x] not in neighbours:
+                    neighbours.append(new_sudoku[n][x])
+            if new_sudoku[y][x] == 0:
+                temp = []
+                for m in range(1,10):
+                    if m not in neighbours:
+                        temp.append(m)
+                if len(temp) == 1:
+                    new_sudoku[y][x] = temp[0]
+                    empty_cells += -1
+                    print(x+1,y+1)
+                    x,y = 9,9
+            x+=1
+        y+=1
+print(new_sudoku)
