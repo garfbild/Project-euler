1f6e90942c81da21bf15874a2628917b0f8c35b6
garfbild <30866252+garfbild@users.noreply.github.com>
2020-07-27 23:33:43 +0100

    Update 96.py

--- solutions/96.py
+++ solutions/96.py
@@ -14,32 +14,9 @@ for i in range(9):
             empty_cells += 1
     new_sudoku.append(new_row)
 
+
+[print(row) for row in new_sudoku]
+print(empty_cells)
+print()
+
 while empty_cells > 0:
-    [print(row) for row in new_sudoku]
-    print(empty_cells)
-    print()
-    y = 0
-    while y <= 8:
-        x=0
-        while x <= 8:
-            neighbours = []
-            for n in range(9):
-                if new_sudoku[y][n] != 0 and new_sudoku[y][n] not in neighbours:
-                    neighbours.append(new_sudoku[y][n])
-                if new_sudoku[n][x] != 0 and new_sudoku[n][x] not in neighbours:
-                    neighbours.append(new_sudoku[n][x])
-            if new_sudoku[y][x] == 0:
-                temp = []
-                for m in range(1,10):
-                    if m not in neighbours:
-                        temp.append(m)
-                if len(temp) == 1:
-                    new_sudoku[y][x] = temp[0]
-                    empty_cells += -1
-                    print(x+1,y+1)
-                    x,y = 9,9
-                if len(temp) == 2:
-                    print("two",x+1,y+1,temp)
-            x += 1
-        y += 1
-print(new_sudoku)
