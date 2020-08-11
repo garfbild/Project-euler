1c8e66891d5a25f010c08e999219b7963140f656
garfbild <30866252+garfbild@users.noreply.github.com>
2020-08-11 03:34:32 +0100

    Update 85.py

--- solutions/85.py
+++ solutions/85.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def Sieve(n):
     sieve = [0] * (n+1)
     for d in range(2,int(n**0.5)+1):
@@ -24,20 +26,23 @@ def primeFactors(n):
             temp = temp / p
     return dict
 
-for m in range(2,20):
+for m in range(6,7):
     primeFactorsList = list(primeFactors(m).items())
     FactorsList = []
     for i in range(len(primeFactorsList)):
         if primeFactorsList[i][1] != 0:
             FactorsList = FactorsList + [primeFactorsList[i][0]]*primeFactorsList[i][1]
 
+    savedPartitions = []
     for n in range(1,2**len(FactorsList)-1):
         heightFactors = []
         widthFactors = []
-        for digit in str(bin(n))[2:].zfill(len(FactorsList)):
-            if digit == "0":
-                heightFactors.append(digit)
+        binaryString = str(bin(n))[2:].zfill(len(FactorsList))
+        for p in range(len(binaryString)):
+            if binaryString[p] == "0":
+                heightFactors.append(FactorsList[p])
             else:
-                widthFactors.append(digit)
-
-    print()
+                widthFactors.append(FactorsList[p])
+        if np.prod(widthFactors) >= np.prod(heightFactors) and [widthFactors,heightFactors] not in savedPartitions:
+            savedPartitions.append([widthFactors,heightFactors])
+            print(m,widthFactors,heightFactors)
