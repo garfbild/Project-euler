f36596ec80a04dd726eff097e601ef52ed541081
garfbild <30866252+garfbild@users.noreply.github.com>
2020-08-30 11:15:36 +0100

    Update 100.py

--- solutions/100.py
+++ solutions/100.py
@@ -1,4 +1,13 @@
 def quadraticSolve(a,b,c):
-    return (-b + (b**2 - 4*a*c)**0.5)/(2*a), (-b - (b**2 - 4*a*c)**0.5)/(2*a)
+    return (-b + (b**2 - 4*a*c)**0.5)/(2*a)
 
-print(quadraticSolve(1,2,1))
+def NFromblue(B):
+    return(1+(8*B**2 - 8*B + 1)**0.5)/2
+
+b = 15
+for n in range(7):
+    print(8*n**2 % 7, 8*n % 7)
+
+# for b in range(1,1000):
+#     if NFromblue(b).is_integer():
+#         print(b,b%7)
