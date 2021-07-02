6f660996e638e4105bebb8b2324e763552e12b79
garfbild <30866252+garfbild@users.noreply.github.com>
2021-07-02 19:07:57 +0100

    hgbvdf

--- solutions/47.py
+++ solutions/47.py
@@ -17,15 +17,41 @@ def primeFactors(n,p):
     primes = p
     dict = {}
     distinctFactors = 0
+    temp = n
     for p in primes:
         dict[p] = 0
-        if n%p == 0:
+        boolvar = False
+        while temp%p == 0:
+            temp = temp/p
             dict[p] += 1
+            boolvar = True
+        if boolvar == True:
             distinctFactors += 1
         if p > n:
-            return dict
-    return distinctFactors
+            return distinctFactors,dict
+    return distinctFactors,dict
 
-primes = Sieve(10000000)
-for i in range(647,1000):
-    print(primeFactors(i,primes))
+def checkDistinct(a,b):
+    #a should be shorter
+    for key in list(a.keys()):
+        if a[key] == b[key] and a[key] != 0:
+            return False
+    return True
+
+n = 10000
+primes = Sieve(n)
+consolidation = []
+
+for i in range(1,n):
+    a = primeFactors(i,primes)
+    if a[0] == 4:
+        #print(i)
+        b = primeFactors(i+1,primes)
+        if b[0] == 4:
+            print(i,i+1)
+            c = primeFactors(i+2,primes)
+            if c[0] == 4:
+                print(i,i+1,i+2)
+                d = primeFactors(i+3,primes)
+                if d[0] == 4:
+                    print(i,a,b,c)
---
+++ solutions/48.py
@@ -0,0 +1,9 @@
+def efficentPower(a,n):
+    temp = a
+    for i in range(n):
+        temp = temp*a
+
+
+print(12345678910%10000000000)
+# for i in range(1000,0,-1):
+#     print(i)
