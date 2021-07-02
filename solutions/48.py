55862c8ecbff629deeb1e4f201edb5e18af8ed42
garfbild <30866252+garfbild@users.noreply.github.com>
2021-07-02 19:31:26 +0100

    jl l

--- solutions/48.py
+++ solutions/48.py
@@ -1,9 +1,13 @@
 def efficentPower(a,n):
     temp = a
-    for i in range(n):
-        temp = temp*a
+    for i in range(n-1):
+        temp = (temp*a)%10000000000
+        #print(str(a**(i+2))[-10:],temp)
+    return temp
 
-
-print(12345678910%10000000000)
-# for i in range(1000,0,-1):
-#     print(i)
+print(str(999**999)[-10:])
+print(efficentPower(999,999))
+s = 0
+for i in range(1000,0,-1):
+     s += efficentPower(i,i)
+print(s,str(s)[-10:])
