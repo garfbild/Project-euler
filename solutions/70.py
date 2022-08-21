e0a98d790185617ff618332029f5adcc47356c01
garfbild <30866252+garfbild@users.noreply.github.com>
2022-08-21 18:34:23 +0100

    Update 70.py

--- solutions/70.py
+++ solutions/70.py
@@ -1,6 +1,6 @@
 import time
 def Sieve(n):
-    sieve = [0] * (n+1)
+    sieve = [0] * int(n+1)
     for d in range(2,int(n**0.5)+1):
         if sieve[d] == 0:
             s = d
@@ -14,13 +14,14 @@ def Sieve(n):
             primes.append(i)
     return primes[2:]
 
-def tau(n,primes):
+def phi(n,primes):
     i = 0
     t = n
-    while primes[i] < n:
+    while primes[i] <= n:
         if n%primes[i] == 0:
             t = t*((primes[i]-1)/primes[i])
-            n = n/primes[i]
+            while n%primes[i] == 0:
+                n = n/primes[i]
         i += 1
     return int(t)
 
@@ -39,31 +40,40 @@ def isPerm(m,n):
             j += 1
         if var == True:
             return False
-    if strN == ["A"]*len(strN):
-        return True
-m = 10**7
-print(isPerm(123,321))
-t1 = time.time()
-primes = Sieve(m)
-t2 = time.time()
-print("primes",t2-t1)
-min = 9999999999999999
-minI = 0
-tauTime = 0
-permTime = 0
-for i in (num for num in range(1,1000) if num not in primes):
-    t1 = time.time()
-    j = tau(i,primes)
-    t2 = time.time()
-    tauTime += (t2-t1)
-    t1 = time.time()
-    if isPerm(i,j):
-        print(i,j)
-        if i/j < min:
-            minI = i
-    t2 = time.time()
-    permTime += (t2-t1)
+    return True
 
-print(minI)
-print("perm",permTime)
-print("tau",tauTime)
+m = 10**7
+primes = Sieve(m/2)#only looked at products of two primes
+
+minV = 9999999999999999
+minN = 0
+p = len(primes)-1
+while p >= 0:
+    q = 0
+    f1 = primes[p]
+    f2 = primes[q]
+    #using eulers product formula it was clear that smallest value was when n was a prime, however, the totient could never permute. Therefore searched all products of two primes. powers have no effect on ratio.
+    n = f1*f2
+    while q < p and n < m:
+        f2 = primes[q]
+        n = f1*f2
+        phiV = (f1-1)*(f2-1)
+        if n/phiV < minV:
+            # isPerm is a time costly function
+            if isPerm(n,phiV):
+                print(n,phiV,n/phiV,f1,f2)
+                minV = n/phiV
+                minN = n
+        q += 1
+    p += -1
+
+#brute force
+# for i in (num for num in range(2,10000) if num not in primes):
+#     j = phi(i,primes)
+#     if isPerm(i,j):
+#         print(i,j)
+#         if i/j < min:
+#             minV = i/j
+#             min = i
+
+print(minV,minN)
